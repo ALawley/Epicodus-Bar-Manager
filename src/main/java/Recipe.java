cecba1718e1a9dac1b12c8a3554c92cf0f939168
Alissa Paluska and Polina Nenchev <polina.mihova@yahoo.com>
2016-03-07 10:40:08 -0800

    recipe test1 pass: all method is empty when initiated

--- src/main/java/Recipe.java
+++ src/main/java/Recipe.java
@@ -8,4 +8,41 @@ public class Recipe {
   private int rating;
   private String creator;
   private String prep_time;
+
+
+  public Recipe (String name, String notes, int rating, String creator, String prep_time) {
+    this.name = name;
+    this.notes = notes;
+    this.rating = rating;
+    this.creator = creator;
+    this.prep_time = prep_time;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public int getRating() {
+    return rating;
+  }
+  public String getCreator() {
+    return creator;
+  }
+
+  public String getPrepTime() {
+    return prep_time;
+  }
+
+  public static List<Recipe> all() {
+  String sql = "SELECT * FROM recipes ORDER BY name";
+  try (Connection con = DB.sql2o.open()) {
+    return con.createQuery(sql).executeAndFetch(Recipe.class);
+  }
+}
+
 }
