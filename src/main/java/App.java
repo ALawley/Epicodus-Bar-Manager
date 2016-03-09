2d6e43ad307c017b437ec51488191211ebb9002d
Anna Lawley <alawley1@gmail.com>
2016-03-08 16:07:08 -0800

    add recipes vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,16 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+    get("/", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    model.put("types", Type.all());
+    model.put("template", "templates/recipes.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+  }
 
 
 }
