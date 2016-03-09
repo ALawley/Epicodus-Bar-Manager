02f7c813730260330766ff59dd7e4ab5b1fc5ffb
Anna Lawley <alawley1@gmail.com>
2016-03-09 09:45:52 -0800

    build recipes page and clean up spark routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,8 +12,53 @@ public class App {
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
       model.put("types", Type.all());
+      model.put("recipes", Recipe.all());
+      model.put("template", "templates/recipes.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/recipes/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      String notes = request.queryParams("notes");
+      String creator = request.queryParams("creator");
+      String preptime = request.queryParams("preptime");
+      String directions = request.queryParams("directions");
+      Recipe newRecipe = new Recipe(name, notes, creator, preptime, directions);
+      newRecipe.save();
+      for (int i = 1; i <= 8; i++) {
+        String amountfield = String.format("amount%d", i);
+        String typefield = String.format("type%d", i);
+        String infofield = String.format("info%d", i);
+        String amountValue = request.queryParams(amountfield);
+        int typeId = Integer.parseInt(request.queryParams(typefield));
+        String info = request.queryParams(infofield);
+        if (amountValue == "" || typeId == 0) {} else {
+          Double amount = Double.parseDouble(amountValue);
+            Ingredient newIngredient = new Ingredient(typeId, amount, info);
+            newIngredient.save();
+            newRecipe.addIngredient(newIngredient.getId());
+        }
+      }
+      model.put("types", Type.all());
+      model.put("recipes", Recipe.all());
+      model.put("template", "templates/recipes.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/planner/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Recipe recipe = Recipe.find(Integer.parseInt(request.params(":id")));
+      model.put("recipe", recipe);
+      model.put("template", "templates/planner.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/inventory", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("types", Type.all());
       model.put("items", Item.all());
-      model.put("template", "templates/recipe.vtl");
+      model.put("template", "templates/inventory.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
@@ -28,20 +73,13 @@ public class App {
       response.redirect("/");
       return null;
     });
-    
-    get("/recipes", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("types", Type.all());
-      model.put("template", "templates/recipes.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
 
-    get("/planner/:id", (request, response) -> {
-      HashMap<String, Object> model = new HashMap<String, Object>();
-      Recipe recipe = Recipe.find(Integer.parseInt(request.params(":id")));
-      model.put("recipe", recipe);
-      model.put("template", "templates/planner.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
+
+
+
+
+
   }
+
+
 }
